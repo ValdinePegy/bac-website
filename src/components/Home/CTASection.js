9cb56ab874da993cd7d25ac0b4ebe8c440e84160
ValdinePegy <pegytchinda@gmail.com>
2025-03-29 10:38:41 -0400

    little modifications requested by the owner

--- src/components/Home/CTASection.js
+++ src/components/Home/CTASection.js
@@ -4,20 +4,41 @@ import { useTranslation } from "react-i18next";
 const CTASection = () => {
   const { t } = useTranslation();
 
+//   return (
+//     <section className="bg-gradient-to-r from-white to-[#FFFAF0] p-8 text-center">
+//       <h2 className="text-3xl font-bold mb-6">{t("home.slogan")}</h2>
+//       <div className="mt-4 flex justify-center gap-4">
+//         <button className="bg-white text-gray-800 border border-gray-300 px-6 py-2 rounded-md shadow-sm hover:bg-gray-100 transition">
+//             {t("home.cta.contact")}
+//         </button>
+//         <button className="bg-green-500 text-white px-6 py-2 rounded-md shadow-sm hover:bg-green-600 transition">
+//             {t("home.cta.appointment")}
+//         </button>
+//     </div>
+//     </section>
+//   );
+// };
   return (
     <section className="bg-gradient-to-r from-white to-[#FFFAF0] p-8 text-center">
       <h2 className="text-3xl font-bold mb-6">{t("home.slogan")}</h2>
       <div className="mt-4 flex justify-center gap-4">
-        <button className="bg-white text-gray-800 border border-gray-300 px-6 py-2 rounded-md shadow-sm hover:bg-gray-100 transition">
-            {t("home.cta.contact")}
-        </button>
+        <a
+          href="https://wa.me/237693824129"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="bg-white text-gray-800 border border-gray-300 px-6 py-2 rounded-md shadow-sm hover:bg-gray-100 transition"
+        >
+          {t("home.cta.contact")}
+        </a>
+
         <button className="bg-green-500 text-white px-6 py-2 rounded-md shadow-sm hover:bg-green-600 transition">
-            {t("home.cta.appointment")}
+          {t("home.cta.appointment")}
         </button>
-    </div>
+      </div>
     </section>
   );
 };
+
 
 
 export default CTASection;
